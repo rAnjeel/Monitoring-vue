7779484178d634df64eb184174dc23c3b70426fe
rAnjeel <rabearisonangelo21@gmail.com>
2025-09-22 11:55:31 +0300

    Impove interface navigation with condition to list devices and ports correctly

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,28 @@
 <template>
-  <ListDevices/>
-  <ListPorts/>
+  <div class="app-shell">
+    <nav class="top-menu">
+      <button class="tab" :class="{ active: activeView === 'devices' }" @click="activeView = 'devices'">
+        <span class="glyphicon glyphicon-hdd" aria-hidden="true" style="margin-right:6px;"></span>
+        Devices
+      </button>
+      <button class="tab" :class="{ active: activeView === 'ports' }" @click="activeView = 'ports'">
+        <span class="glyphicon glyphicon-transfer" aria-hidden="true" style="margin-right:6px;"></span>
+        Ports
+      </button>
+    </nav>
+
+    <section>
+      <ListDevices v-if="activeView === 'devices'" />
+      <ListPorts v-else />
+    </section>
+  </div>
 </template>
 
 <script setup>
+  import { ref } from 'vue';
   import ListDevices from './views/ListDevices.vue';
   import ListPorts from './views/ListPorts.vue';
-</script>
+  import './assets/App.css';
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-</style>
+  const activeView = ref('devices');
+</script>
